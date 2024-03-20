ce7ed9cc34f6df42cd7b182dd9333ba5021364d9
u1000e <73210038+u1000e@users.noreply.github.com>
2024-03-20 12:31:25 +0900

    응 나 숨쉬었어~ - 홍길동
    12312321313

--- LunchTime/src/com/kh/semi/admin/controller/AdminController.java
+++ LunchTime/src/com/kh/semi/admin/controller/AdminController.java
@@ -5,7 +5,15 @@ public class AdminController {
 	
 	public void hiAdmin() {
 		System.out.println("안녕 나는 어드민이야~!");
-		
+	}
+	
+	public void a() {
+		System.out.println("호허허허허히히히히");
+	}
+	
+	
+	public void b() {
+		System.out.println("나는 참 일을ㅇ 려심히하는사람이야~~~");
 	}
 
 }
